beaf4d0c83fce41f7d01d21a7b6ae06efa8c5194
edencodes <118276354+EdenTheDev@users.noreply.github.com>
2024-05-14 16:40:56 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function() {
         videoOverlay.style.display = 'none'; // Hide the video overlay
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('.fullscreen-image');
+    images.forEach(function(image) {
+        image.addEventListener('click', function() {
+            var videoOverlay = document.getElementById('videoOverlay');
+            var videoPlayer = document.getElementById('videoPlayer');
+            videoOverlay.style.display = 'flex'; // Show the video overlay
+            videoPlayer.play();
+        });
+    });
+
+    var videoOverlay = document.getElementById('videoOverlay');
+    videoOverlay.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent click from propagating to other elements
+        var videoPlayer = document.getElementById('videoPlayer');
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+        videoOverlay.style.display = 'none'; // Hide the video overlay
+    });
+});
